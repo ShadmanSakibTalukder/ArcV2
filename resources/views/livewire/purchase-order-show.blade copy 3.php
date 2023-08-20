dd3ed4fdfba427212e50579bd4cab28d40ab5105
RasulaMakbul <emu.cfs@gmail.com>
2023-08-20 17:55:14 +0600

    purchase order error

--- resources/views/livewire/purchase-order-show.blade copy 3.php
+++ resources/views/livewire/purchase-order-show.blade copy 3.php
@@ -33,11 +33,16 @@
                             </tr>
                         </thead>
                         <tbody>
+
                             @forelse ($added_to_list as $item)
+
                             <tr>
                                 <td>{{ $item->parts_added_inlist->requested_part_no }}</td>
                                 <td>{{ $item->parts_added_inlist->requested_nomenclature }}</td>
+
                                 <td>{{ $item->qty }}</td>
+
+
                                 <td>
                                     <select class="form-select sm" wire:model="selectedOption.{{ $item->id }}" wire:change="updateVendorPrices({{ $item->id }})">
                                         <option value="">Select Price</option>
@@ -47,13 +52,14 @@
                                         <option value="">No Price Available</option>
                                         @endforelse
                                     </select>
+                                </td> {{ $this->calculateUnitPrice($item) }}
                                 </td>
-                                <td>{{ $this->calculateUnitPrice($item) }}</td>
                                 <td>
-                                    {{ $this->calculateTotalPrice($item) }}
+                                    {{$this->calculateTotalPrice($item)}}
                                     @php
-                                    $subTotal += $this->calculateTotalPrice($item);
-                                    $declaredTotal += $this->calculateDeclaredTotalPrice($item);
+                                    $subTotal+=$this->calculateTotalPrice($item);
+                                    $declaredTotal+=$this->calculateDeclaredTotalPrice($item);
+
                                     @endphp
                                 </td>
                                 <td>
@@ -86,7 +92,57 @@
                 </div>
 
                 <form wire:submit.prevent="codOrder">
-                    <!-- ... Form fields ... -->
+                    <div class="mb-3">
+                        <label for="po_no" class="form-label">Purchase Order No:</label>
+                        <input type="text" class="form-control" id="po_no" wire:model.defer="po_no" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="company" class="form-label">Company Name:</label>
+                        <input type="text" class="form-control" id="company" wire:model.defer="company" required>
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label>Company Address</label>
+                        <textarea name="company_address" id="company_address" class="form-control" wire:model.defer="company_address" rows="2" required></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="buyer_name" class="form-label">Buyer Name:</label>
+                        <input type="text" class="form-control" id="buyer_name" wire:model.defer="buyer_name" required>
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label>Buyer Address</label>
+                        <textarea name="buyer_address" id="buyer_address" class="form-control" wire:model.defer="buyer_address" rows="2" required></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="vendor_name" class="form-label">Vendor Name:</label>
+                        <input type="text" class="form-control" id="vendor_name" name="vendor_name" wire:model.defer="vendor_name" required>
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label>Vendor Address</label>
+                        <textarea name="vendor_address" id="vendor_address" class="form-control" rows="2" wire:model.defer="vendor_address" required></textarea>
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label>Shipping Address</label>
+                        <textarea name="shipping_address" id="shipping_address" class="form-control" rows="2" wire:model.defer="shipping_address" required></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="tender_no" class="form-label">Tender No:</label>
+                        <input type="text" class="form-control" id="tender_no" wire:model.defer="tender_no" name="tender_no" required>
+                    </div>
+
+
+                    <div class="mb-3">
+                        <label for="po_date" class="form-label">Purchase Order Date:</label>
+                        <input type="date" class="form-control" id="po_date" wire:model.defer="po_date" name="po_date" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <input type="hidden" name="subTotal" value="{{ $subTotal }}">
+
+                    </div>
+                    <div class="mb-3">
+                        <input type="hidden" name="declaredTotal" value="{{ $declaredTotal }}">
+
+                    </div>
                     <div class="my-5 d-flex justify-content-end p-3">
                         <button type="submit" class="btn btn-md btn-outline-primary px-3 mx-2">
                             <span wire:loading.remove wire:target="codOrder">Save</span>
@@ -94,6 +150,7 @@
                         </button>
                         <a href="{{ route('purchased_order.index') }}" class="btn btn-md btn-outline-secondary">Back</a>
                     </div>
+
                 </form>
             </div>
         </div>
